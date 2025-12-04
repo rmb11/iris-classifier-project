8b14e561ade1e83a451a554933e0dbac044ae59e
rmb11 <rbezant@gmail.com>
2025-12-04 13:10:10 +0800

    feat: Add basic /api/predict endpoint and placeholder view

--- classifier/views.py
+++ classifier/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from ml.predict import predict
 
 # Create your views here.
+@csrf_exempt
+def predict_api(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "Only POST is allowed."}, status=405)
+
+    # ML prediction to go here later
+    return JsonResponse({"message": "Predict API is wired up!"})
